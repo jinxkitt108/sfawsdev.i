d39f811ad691b5af07388a85791474550cb11a9c
unknown <clint.stupra@gmail.com>
2019-12-02 21:10:28 +0800

    Procfile for Heroku

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Overtrue\LaravelFollow\FollowServiceProvider::class,
+        Vimeo\Laravel\VimeoServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -225,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Vimeo' => Vimeo\Laravel\Facades\Vimeo::class,
 
     ],
 
